b5cee7bf64208ecc4097f5d0474efac1580dc81e
Savushkin Sergey <savushkinsv@mail.ru>
2024-09-10 20:09:46 +0300

    add folder db

---
+++ src/main/java/com/example/db/ConnectionManagerImpl.java
@@ -0,0 +1,72 @@
+package com.example.db;
+
+import com.example.exception.DataBaseDriverLoadException;
+import com.example.util.PropertiesUtil;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Устанавливает соединение с базой данных.
+ */
+public class ConnectionManagerImpl implements IConnectionManager {
+    private static ConnectionManagerImpl instance;
+    private static final String DB_DRIVER_CLASS_NAME_KEY = "db.driver-class-name";
+    private static final String DB_PORT_KEY = "db.local_port";
+    private static final String DB_NAME_KEY = "db.name";
+    private static final String DB_URL_KEY = "db.url";
+    private static final String DB_URL = PropertiesUtil.getProperty(DB_URL_KEY) +
+            PropertiesUtil.getProperty(DB_PORT_KEY) + "/" + PropertiesUtil.getProperty(DB_NAME_KEY);
+    private static final String DB_USER_KEY = "db.user";
+    private static final String DB_PASSWORD_KEY = "db.password";
+
+
+    private ConnectionManagerImpl() {
+    }
+
+    /**
+     * Возвращает объект класса {@code ConnectionDB}.
+     *
+     * @return объект класса
+     */
+    public static IConnectionManager getInstance() {
+        if (instance == null) {
+            instance = new ConnectionManagerImpl();
+            loadDriver(PropertiesUtil.getProperty(DB_DRIVER_CLASS_NAME_KEY));
+        }
+
+        return instance;
+    }
+
+    /**
+     * Загружает драйвер класса.
+     *
+     * @param driverClass имя драйвера класса
+     * @throws ClassNotFoundException если драйвер класса не найден
+     */
+    private static void loadDriver(String driverClass) {
+        try {
+            Class.forName(driverClass);
+            System.out.println("Database driver loaded.");
+        } catch (ClassNotFoundException e) {
+            throw new DataBaseDriverLoadException("Database driver not loaded.");
+        }
+    }
+
+    /**
+     * Устанавливает соединение с базой данных.
+     *
+     * @return соединение с базой данных
+     * @throws ClassNotFoundException если класс не найден
+     * @throws SQLException           если произошла ошибка доступа к базе данных или URL-адрес имеет значение NULL
+     */
+    public Connection getConnection() throws SQLException {
+
+        return DriverManager.getConnection(
+                DB_URL,
+                PropertiesUtil.getProperty(DB_USER_KEY),
+                PropertiesUtil.getProperty(DB_PASSWORD_KEY)
+        );
+    }
+}
