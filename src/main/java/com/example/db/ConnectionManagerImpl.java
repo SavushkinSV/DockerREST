621c22916299bd0b435a85266fef976d3ce1bbf0
SavushkinSV <savushkin.sv@yandex.ru>
2024-09-15 17:15:13 +0300

    feat: add getConnectionTest()

--- src/main/java/com/example/db/ConnectionManagerImpl.java
+++ src/main/java/com/example/db/ConnectionManagerImpl.java
@@ -12,13 +12,18 @@ import java.sql.SQLException;
 public class ConnectionManagerImpl implements IConnectionManager {
     private static ConnectionManagerImpl instance;
     private static final String DB_DRIVER_CLASS_NAME_KEY = "db.driver-class-name";
-    private static final String DB_PORT_KEY = "db.local_port";
+
+    private static final String DB_LOCAL_URL_KEY = "db.local_url";
+    private static final String DB_CONTAINER_URL_KEY = "db.container_url";
+
+    private static final String DB_LOCAL_PORT_KEY = "db.local_port";
+    private static final String DB_CONTAINER_PORT_KEY = "db.container_port";
+
     private static final String DB_NAME_KEY = "db.name";
-    private static final String DB_URL_KEY = "db.url";
-    private static final String DB_URL = PropertiesUtil.getProperty(DB_URL_KEY) +
-            PropertiesUtil.getProperty(DB_PORT_KEY) + "/" + PropertiesUtil.getProperty(DB_NAME_KEY);
     private static final String DB_USER_KEY = "db.user";
     private static final String DB_PASSWORD_KEY = "db.password";
+
+    private Connection connection;
 
     private ConnectionManagerImpl() {
     }
@@ -58,14 +63,40 @@ public class ConnectionManagerImpl implements IConnectionManager {
      *
      * @return соединение с базой данных
      * @throws ClassNotFoundException если класс не найден
-     * @throws SQLException           если произошла ошибка доступа к базе данных или URL-адрес имеет значение NULL
+     *                                * @throws SQLException           если произошла ошибка доступа к базе данных или URL-адрес имеет значение NULL
      */
-    public Connection getConnection() throws SQLException {
+    public Connection getConnection() {
+        String url = PropertiesUtil.getProperty(DB_CONTAINER_URL_KEY) +
+                PropertiesUtil.getProperty(DB_CONTAINER_PORT_KEY) + "/" +
+                PropertiesUtil.getProperty(DB_NAME_KEY);
 
-        return DriverManager.getConnection(
-                DB_URL,
-                PropertiesUtil.getProperty(DB_USER_KEY),
-                PropertiesUtil.getProperty(DB_PASSWORD_KEY)
-        );
+        try {
+            connection = DriverManager.getConnection(
+                    url,
+                    PropertiesUtil.getProperty(DB_USER_KEY),
+                    PropertiesUtil.getProperty(DB_PASSWORD_KEY)
+            );
+        } catch (SQLException e) {
+            connection = getConnectionTest();
+        }
+
+        return connection;
+    }
+
+    @Override
+    public Connection getConnectionTest() {
+        String url = PropertiesUtil.getProperty(DB_LOCAL_URL_KEY) +
+                PropertiesUtil.getProperty(DB_LOCAL_PORT_KEY) + "/" +
+                PropertiesUtil.getProperty(DB_NAME_KEY);
+        try {
+            connection = DriverManager.getConnection(
+                    url,
+                    PropertiesUtil.getProperty(DB_USER_KEY),
+                    PropertiesUtil.getProperty(DB_PASSWORD_KEY)
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
     }
 }
